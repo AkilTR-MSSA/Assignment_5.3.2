ffe4e8f810ebfa7d20f066e062cb19d03bd53fc3
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-07 16:01:19 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(ClimbStairs(2));
+Console.WriteLine(ClimbStairs(3));
+Console.WriteLine(ClimbStairs(5));
+
+int ClimbStairs(int n)
+{
+    int a = 1;
+    int b = 1;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
